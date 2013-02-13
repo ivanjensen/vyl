1694830357041e2f3524420f6a5c9c15916b32f9
Ivan Jensen <ivan.jensen@elasticpath.com>
2013-02-13 09:40:52 -0800

    lots of tidy up and added a bunch more tests.  Changed from 'my' test framework to Test::Simple

--- test/test.pl
+++ test/test.pl
@@ -1,12 +1,14 @@
 #!/usr/bin/perl -Wall
 use Cwd;
-use Test::Simple tests => 2;
+use Test::Simple tests => 6;
 
 ok(&test("find ./files -name test.txt", "vi ./files/test.txt"), 'single result, no argument');
 ok(&test("find ./files -name myfile", "vi ./files/dir1/myfile", "1"), 'single result, specify first');
 ok(&test("find ./files -name myfile", "vi ./files/dir1/myfile"), 'multiple results, no argument, should take first');
 ok(&test("find ./files -name myfile", "vi ./files/dir1/myfile", "1"), 'multiple results, select first result');
 ok(&test("find ./files -name myfile", "vi ./files/myfile", "2"), 'multiple results, select second result');
+ok(&test("ls -1 files/quotes", "vi filewith\"adoublequote", "1"), 'result with a double quote');
+ok(&test("ls -1 files/quotes", "vi filewith'asinglequote", "2"), 'result with a double quote');
 
 
 sub test {
@@ -19,7 +21,7 @@ sub test {
 	# send the command we want to pretend was last in history
 	delete $ENV{"EDITOR"};
 	$ENV{"VYL_DEBUG"} = $command;
-	system("../../vyl", $vyl_args);
+	system("../vyl", $vyl_args);
 
 	open FILE, "<", "test-result.txt" or die $!;
 
